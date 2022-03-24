56cf6164867faa23f2d2b3b8ff9cfd7112a14164
ankitMukhija <64611103+ankitMukhija@users.noreply.github.com>
2022-03-24 11:08:12 -0700

    Update bq_sql.sql

--- bq_sql.sql
+++ bq_sql.sql
@@ -1,3 +1,6 @@
-Drop table `sada-ankit.test_workflow.covid_data`;
-CREATE TABLE `sada-ankit.test_workflow.covid_data_1`
+DECLARE project STRING DEFAULT 'sada-ankit';
+DECLARE dataset STRING DEFAULT 'test_workflow';
+DECLARE table_id STRING DEFAULT 'covid_data_1'
+Drop table project.dataset.table_id;
+CREATE TABLE project.dataset.table_id
 AS SELECT * FROM `sada-ankit.test.covid19_jhu_csse_part` WHERE date = '2020-07-23';
